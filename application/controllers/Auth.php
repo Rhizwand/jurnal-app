2d812bb5dd67bb5192d57022e11cf3196a4245fa
Rhizwand <riswandir461311@gmail.com>
2021-04-21 07:55:38 +0800

    Menambahkan fitur regstrasi

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,13 +3,37 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('Auth_model', 'auth');
+  }
 
   public function index()
   {
-    echo 'Hello world';
+    $data['title'] = 'Login';
+    $this->load->view('auth-template/header', $data);
+    $this->load->view('auth/index');
+    $this->load->view('auth-template/footer');
   }
-  public function about()
+  public function register()
   {
-    echo "auth/about";
+    $this->form_validation->set_rules('username', 'Username', 'trim|required');
+    $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[user.email]');
+    $this->form_validation->set_rules('password1', 'Password', 'required|trim|matches[password2]|min_length[6]', [
+      'min_length' => 'must be at least 6 character',
+      'matches' => 'password does not matches'
+    ]);
+    $this->form_validation->set_rules('password2', 'Password', 'trim|required|matches[password1]');
+    if ($this->form_validation->run() == false) {
+      $data['title'] = 'Register';
+      $this->load->view('auth-template/header', $data);
+      $this->load->view('auth/register');
+      $this->load->view('auth-template/footer');
+    } else {
+      $this->auth->inser_user();
+      $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulations, your account has created. Please Login</div>');
+      redirect('auth');
+    }
   }
 }
---
+++ application/models/Auth_model.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth_model extends CI_Model
+{
+
+  public function inser_user()
+  {
+    $data = [
+      'username' => htmlspecialchars($this->input->post('username', true)),
+      'email' => htmlspecialchars($this->input->post('email', true)),
+      'password' => password_hash($this->input->post('password1', true), PASSWORD_DEFAULT),
+      'is_active' => 1,
+      'date_created' => time()
+    ];
+    $this->db->insert('user', $data);
+  }
+}
